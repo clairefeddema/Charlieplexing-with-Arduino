bd0da63cd6c948af3238c71b2d68bae27d16e682
clairefeddema <67806894+clairefeddema@users.noreply.github.com>
2020-08-17 00:07:59 -0700

    Update CharlieplexingClickerCounter.ino

--- CharlieplexingClickerCounter.ino
+++ CharlieplexingClickerCounter.ino
@@ -1,30 +1,67 @@
-int buttonPin = 13;
-int c1 = 12;
-int c2 = 8;
-int c3 = 7;
-int c4 = 4;
+/* Arduino Clicker Counter Using Charlieplexing 
+ * 
+ * The Arduino Clicker Counter consists of an Arduino Uno board connected to a breadboard 
+ * with seven LED diodes arranged in the pattern of a seven-segment display and one button. 
+ * 
+ * The display initially displays the number zero, and each time the button is pressed, the 
+ * counter advances by one number. Once the counter reaches nine and is clicked again, 
+ * the display rolls over back to zero. 
+ * 
+ * Charlieplexing is used to decrease the number of pins needed on the Arduino from
+ *  7 to 4 by changing pin modes as well as high/low statuses of each pin. It allows 
+ *  current to flow in the correct and unique direction for each LED. 
+ *
+ *  Written by Claire Feddema on July 13 2020
+ */
 
+
+// Declarations 
+
+// Pin taking button imput
+int buttonPin = 13;
+
+//Pins connected indirectly to display LEDs using charlieplexing
+int c1 = 5;
+int c2 = 4;
+int c3 = 3;
+int c4 = 2;
+
+//Delay variables in milliseconds
 int delayTime = 100;
 int d = 2;
 
+
+//Variables to keep track of the button's status (pressed or not) currently and 
+// previously to compare (in order to know when it goes from pressed to unpressed, 
+// which counts as a single button press) 
 int buttonNew;
 int buttonOld = 1; 
 
 int count = 0; 
 
+//Runs once when the Arduino is powered up
+//Begins console 
 void setup() {
-  // put your setup code here, to run once:
 Serial.begin(9600);
 }
 
+// Runs as long as the Arduino stays plugged in 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
+// Reads the state of the button and sets buttonNew to 1 when unpressed or 0 when pressed 
+// then checks when the button goes from pressed to unpressed, and adds to the count of button presses
+// Delays by the set delayTime and prints the count to the console
 buttonNew = digitalRead(buttonPin);
   if(buttonOld == 0 && buttonNew == 1){
     count ++;
     delay(delayTime);
     Serial.println(count); 
   }
+
+//Calls the correct functions for each individual LED of the display 
+// in quick succession to turn on the correct LEDs and display the numbers
+// 0 through 9 based on the value of the count variable 
 if(count == 0) {
   led1 ();
   delay(d);
@@ -141,17 +178,19 @@ if(count == 9) {
   led6 (); 
   delay(d);
 }
+// Rolls the display back to 0 to fit in a single digit display 
 if(count == 10) {
   count = 0;
 }
   
-  
+// Sets the previous value of the button status to the new value to be compared in 
+// the next loop iteration  
 buttonOld = buttonNew;
 }
 
 
-
-
+// Changes both pin modes and statuses of each pin to turn on each
+// of the 7 seperate LEDs on the  display
 void led1 () {
   pinMode(c1, OUTPUT);
   pinMode(c2, OUTPUT);
